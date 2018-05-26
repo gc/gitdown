45afc9177ea47a00ea7c3336c77cc433f2592983
gc <30398469+gach@users.noreply.github.com>
2018-05-26 17:34:01 +1000

    use CDN for css layout lib

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
-import './assets/blueprint.css';
+//import './assets/blueprint.css';
 import './assets/github-markdown.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
