12540547a67ec7e9768dc7e5683f049ac4a58c75
gc <30398469+gach@users.noreply.github.com>
2018-08-24 00:29:24 +1000

    cut things down

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 class App extends Component {
   state = {
-    markdown: localStorage.getItem('markdown')
+    markdown: localStorage.getItem('markdown') || `# Title`
   }
 
-  handleChange = (event) => {
-    const markdown = event.target.value;
+  handleChange = (e) => {
+    const markdown = e.target.value;
     localStorage.setItem('markdown', markdown);
-    this.setState({markdown});
+    this.setState({ markdown });
   }
 
   link(text, link) {
     return (
-      <a className="link" href={link} target="_blank" rel="noopener noreferrer">
+      <a href={link} target="_blank" rel="noopener noreferrer">
         {text}
       </a>
     );
@@ -22,14 +22,16 @@ class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        {this.link('Github', 'https://github.com/gc/gitdown')} /
-        {this.link('Guide', 'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet')}
-        <div bp="grid 6" className="App">
-            <textarea value={this.state.markdown} onChange={this.handleChange} />
-            <ReactMarkdown source={this.state.markdown} className="markdown-body"/>
+      <Fragment>
+        <div style={{ position: 'absolute', top: 5, left: 5, fontSize: 14 }}>
+          {this.link('Github', 'https://github.com/gc/gitdown')} /
+          {this.link('Guide', 'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet')}
         </div>
-      </React.Fragment>
+        <div style={{ display: 'flex', height: '100%' }}>
+            <textarea style={{ height: '100%', width: '35%' }} value={this.state.markdown} onChange={this.handleChange} />
+            <ReactMarkdown source={this.state.markdown} className="md"/>
+        </div>
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './assets/index.css';
 import './assets/github-markdown.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<App />, document.getElementById('root'));
